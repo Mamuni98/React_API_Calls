29b3405fcbb1282c3e6721b3c99aaa9e359788eb
Mamuni98 <nabanitasahoo468@gmail.com>
2023-05-20 11:37:25 +0530

    LoadingPage

--- src/App.js
+++ src/App.js
@@ -5,28 +5,40 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  async function fetchMoviesHandler() {
-    const response = await fetch("https://swapi.dev/api/films/");
+  const [isLoading, setIsLoading] = useState(false);
+  const fetchMoviesHandler = async () => {
+    try {
+      setIsLoading(true);
+      const response = await fetch("https://swapi.dev/api/films/");
 
-    const data = await response.json();
+      const data = await response.json();
 
-    const transformedMovies = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-      };
-    });
-    setMovies(transformedMovies);
-  }
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+        };
+      });
+      setIsLoading(false);
+      setMovies(transformedMovies);
+    } catch (error) {
+      console.log(error);
+    }
+    // setIsLoading(false);
+  };
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section>
-        <MoviesList movies={movies} />
-      </section>
+      {isLoading ? (
+        <h4>...Loading</h4>
+      ) : (
+        <section>
+          <MoviesList movies={movies} />
+        </section>
+      )}
     </React.Fragment>
   );
 }
